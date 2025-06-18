4d76eafb22bc16e8b4b12e2cc71a805e1a182ee8
Alfredo <alfredo.centeno.garcia27@gmail.com>
2025-06-18 18:16:08 -0500

    feat: add fetch from API and from Mocked

---
+++ src/components/InboxScreen.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import InboxScreen from './InboxScreen';
+import store from '../lib/store';
+import { http, HttpResponse } from 'msw';
+import { MockedState } from './TaskList.stories';
+import { Provider } from 'react-redux';
+
+const meta: Meta<typeof InboxScreen> = {
+  component: InboxScreen,
+  title: 'InboxScreen',
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+          return HttpResponse.json(MockedState.tasks);
+        }),
+      ],
+    },
+  },
+};
+
+export const Error: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+          return new HttpResponse(null, {
+            status: 403,
+          });
+        }),
+      ],
+    },
+  },
+};
